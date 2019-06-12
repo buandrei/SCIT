75556cb0e99b7d017b673c19e3e7ab7c33c8dd8b
Bu Andrei <andreibu.ancora@gmail.com>
2019-06-12 19:21:13 +0300

    Primul test calumea

--- oopConcepts/src/sci/feature1/Main.java
+++ oopConcepts/src/sci/feature1/Main.java
@@ -2,8 +2,6 @@ package sci.feature1;
 
 public class Main {
     public static void main(String args[]){
-        System.out.println("Ceva teste pe aici!");
-        System.out.println("Ceva teste pe aici!");
-        System.out.println("Ceva teste pe aici!");
+        System.out.println("Facem primul test cu feature1!");
     }
 }
