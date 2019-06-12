3cd66fb0db53fa1e2c68fa9f5ef55d5869e3f449
Bu Andrei <andreibu.ancora@gmail.com>
2019-06-12 19:32:54 +0300

    alandala

--- oopConcepts/src/sci/feature1/Main.java
+++ oopConcepts/src/sci/feature1/Main.java
@@ -2,7 +2,11 @@ package sci.feature1;
 
 public class Main {
     public static void main(String args[]){
-
+        System.out.println("Alandala");
+        System.out.println("Alandala");
+        System.out.println("Alandala");
+        System.out.println("Alandala");
+        System.out.println("Alandala");
 
     }
 }
